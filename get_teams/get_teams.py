3854c559d56d6a5490bfe9ebb1d9903d79b59458
Jason Sevilla <jasonsevilla@Jasons-MacBook-Pro.local>
2020-07-09 01:49:52 -0400

    Able to get team stats for One team

--- get_teams/get_teams.py
+++ get_teams/get_teams.py
@@ -18,3 +18,23 @@ def get_all_teams():
     responseJson = json.dumps(responseJson, indent=4)
 
     print(responseJson)
+
+
+def get_all_teams_stats():
+    url = "https://api-basketball.p.rapidapi.com/statistics"
+
+    querystring = {"league": "12", "season": "2019-2020", "team": "133"}
+    response = requests.request(
+        "GET", url, headers=headers, params=querystring)
+
+    responseJson = response.json()
+    print(responseJson['response']['team'])
+    # if responseJson["errors"]["team"] != "The Team field do not exist.":
+    responseJson = json.dumps(responseJson, indent=4)
+    # print(responseJson["errors"]["teams"])
+
+    #        if responseJson["errors"]["teams"] != "The Team field do not exist.":
+    print(responseJson)
+
+
+get_all_teams_stats()
